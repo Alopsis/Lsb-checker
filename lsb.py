08249d4f2d6b6fda57b49e0cc2f73163668d8206
Alopsis <zaxo.moulager@gmail.com>
2025-11-28 14:36:08 +0100

    ajout du mode carré

--- lsb.py
+++ lsb.py
@@ -22,11 +22,13 @@ print("-------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description="Script avec une option -p ou --pas")
 parser.add_argument("-p", "--pas", type=int, default=1, help="Option pour spécifier un pas")
 parser.add_argument("-i","--input", type=str,required=True, help="Image d'entrée")
-parser.add_argument("-d","--direction", type=str,default="droite", help="Direction des LDB (droite,bas,diagonale)")
+parser.add_argument("-d","--direction", type=str,default="droite", help="Direction des LDB (droite,bas,diagonale,carre)")
 parser.add_argument("-b","--bits", type=str, default="a" , help="LSB des bits voulus (r,g,b,a)")
 parser.add_argument("-bf", "--brute-force", action="store_true", help="Mode brute force")
 parser.add_argument("-pX","--pixelX", type=int, default=0, help="Position X du pixel de départ")
 parser.add_argument("-pY","--pixelY", type=int, default=0, help="Position Y du pixel de départ")
+parser.add_argument("-largeur","--largeur", type=int, default=0, help="Largeur du message a extraire (0 = tout)")
+parser.add_argument("-hauteur","--hauteur", type=int, default=0, help="Hauteur du message a extraire (0 = tout)")
 args = parser.parse_args()
 
 # Fonctions 
@@ -44,8 +46,13 @@ def check_arg():
     bits = args.bits
     pX = args.pixelX
     pY = args.pixelY
+    largeur = args.largeur
+    hauteur = args.hauteur  
     if pX < 0 or pY < 0:
         print("Les coordonnées des pixels doivent être positives.")
+        usage()
+    if largeur < 0 or hauteur < 0:
+        print("La largeur et la hauteur doivent être positives.")
         usage()
     if not file_exists(inputFile):
         print("Votre fichier n'existe pas.")
@@ -58,7 +65,7 @@ def check_arg():
 
     if step < 0 or step > 100:
         usage()
-    if direction not in ["droite", "bas", "diagonale"]:
+    if direction not in ["droite", "bas", "diagonale","carre"]:
         usage()
     if bits not in ["r","g","b","a"]:
         usage()
@@ -126,42 +133,73 @@ def extract_lsb(image_path, step, direction, bit, pX, pY):
     img = cv2.imread(image_path)
     bits = []
     height, width = img.shape[0], img.shape[1]
-    for k in range(0, 999999999, step):  # boucle très large car break manuel
-        if direction == "droite":
-            x = pX
-            y = pY + k
-            if y >= width:
-                break
-        elif direction == "bas":
-            x = pX + k
-            y = pY
-            if x >= height:
-                break
-        elif direction == "diagonale":
-            x = pX + k
-            y = pY + k
-            if x >= height or y >= width:
-                break
-        else:
-            print("Direction inconnue")
+    if largeur == 0:
+        max_largeur = width
+    else:
+        max_largeur = min(pY + largeur, width)
+    if hauteur == 0:
+        max_hauteur = height
+    else:
+        max_hauteur = min(pX + hauteur, height)
+    if direction == "carre":
+        if largeur == 0 or hauteur == 0:
+            print("ERREUR : Pour le mode 'carre', vous devez spécifier une largeur et une hauteur non nulles")
             return
-        pixel = img[x, y]
-        lsb_blue  = pixel[0] & 1
-        lsb_green = pixel[1] & 1
-        lsb_red   = pixel[2] & 1
-        if bit == "r":
-            bits.append(str(lsb_red))
-        elif bit == "g":
-            bits.append(str(lsb_green))
-        elif bit == "b":
-            bits.append(str(lsb_blue))
-        elif bit == "a":
-            bits.append(str(lsb_red))
-            bits.append(str(lsb_green))
-            bits.append(str(lsb_blue))
-    text = ''.join(bits)
-    print("Voici le message décodé :")
-    print(decode_binary_strings(text))
+        
+        for x in range(pX, max_hauteur, step):
+            for y in range(pY, max_largeur, step):
+                pixel = img[x, y]
+                lsb_blue  = pixel[0] & 1
+                lsb_green = pixel[1] & 1
+                lsb_red   = pixel[2] & 1
+                
+                if bit == "r":
+                    bits.append(str(lsb_red))
+                elif bit == "g":
+                    bits.append(str(lsb_green))
+                elif bit == "b":
+                    bits.append(str(lsb_blue))
+                elif bit == "a":
+                    bits.append(str(lsb_red))
+                    bits.append(str(lsb_green))
+                    bits.append(str(lsb_blue))
+    else:
+        for k in range(0, 999999999, step):  # boucle très large car break manuel
+            if direction == "droite":
+                x = pX
+                y = pY + k
+                if y >= max_largeur:
+                    break
+            elif direction == "bas":
+                x = pX + k
+                y = pY
+                if x >= max_hauteur:
+                    break
+            elif direction == "diagonale":
+                x = pX + k
+                y = pY + k
+                if x >= max_hauteur or y >= max_largeur:
+                    break
+            else:
+                print("Direction inconnue")
+                return
+            pixel = img[x, y]
+            lsb_blue  = pixel[0] & 1
+            lsb_green = pixel[1] & 1
+            lsb_red   = pixel[2] & 1
+            if bit == "r":
+                bits.append(str(lsb_red))
+            elif bit == "g":
+                bits.append(str(lsb_green))
+            elif bit == "b":
+                bits.append(str(lsb_blue))
+            elif bit == "a":
+                bits.append(str(lsb_red))
+                bits.append(str(lsb_green))
+                bits.append(str(lsb_blue))
+        text = ''.join(bits)
+        print("Voici le message décodé :")
+        print(decode_binary_strings(text))
 
 
 
